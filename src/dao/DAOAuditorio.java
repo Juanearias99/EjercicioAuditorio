e6e9b58a7e8396dcd6056215abfef69cc09515e2
Juanearias99 <arias.juan.6984@eam.edu.co>
2025-07-18 15:53:10 -0500

    add the class DAOAuditorio

--- src/dao/DAOAuditorio.java
+++ src/dao/DAOAuditorio.java
@@ -4,10 +4,27 @@
  */
 package dao;
 
+import modelo.Auditorio;
+import singleton.Singleton;
+
 /**
  *
  * @author Juan Esteban
  */
 public class DAOAuditorio {
-    
+
+    private Auditorio[][] auditorios;
+
+    public DAOAuditorio() {
+        this.auditorios = Singleton.getINSTANCIA().getAuditorios();
+
+    }
+
+    public Auditorio obtenerAuditorios(int fila, int columna) {
+        return auditorios[fila][columna];
+    }
+
+    public boolean auditorioOcupado(int fila, int columna) {
+        return obtenerAuditorios(fila, columna).hayTema();
+    }
 }
