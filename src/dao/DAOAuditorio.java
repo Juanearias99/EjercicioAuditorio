fdb799a0412ebdb0e5c7ef1b82f3c417fcd1a23e
Juanearias99 <arias.juan.6984@eam.edu.co>
2025-10-16 21:28:58 -0500

    Se le agrego dos metodos que es obtenerAuditorio, auditorioOcupado en la clase DAOAuditorio

--- src/dao/DAOAuditorio.java
+++ src/dao/DAOAuditorio.java
@@ -4,9 +4,26 @@
  */
 package dao;
 
+import modelo.Auditorio;
+import persistencia.Persistencia;
+
 /**
  *
  * @author Juan Esteban
  */
-public class DAOAuditorio {    
+public class DAOAuditorio {
+
+    private Auditorio[][] auditorio;
+
+    public DAOAuditorio() {
+        this.auditorio = Persistencia.getInstancia().getAuditorios();
+    }
+
+    public Auditorio obtenerAuditorios(int fila, int columna) {
+        return this.auditorio[fila][columna];
+    }
+
+    public boolean auditoriosOcupados(int fila, int columna) {
+        return obtenerAuditorios(fila, columna).validarPersona();
+    }
 }
