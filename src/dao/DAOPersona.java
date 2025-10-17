9d290b4ee50c5e21a5b137308681732874473072
Juanearias99 <arias.juan.6984@eam.edu.co>
2025-10-17 07:42:05 -0500

    En la clase DAOPersona se hizo un crud

--- src/dao/DAOPersona.java
+++ src/dao/DAOPersona.java
@@ -4,10 +4,69 @@
  */
 package dao;
 
+import idao.IDAOPersona;
+import modelo.Auditorio;
+import modelo.Persona;
+import persistencia.Persistencia;
+import util.LSE;
+
 /**
  *
  * @author Juan Esteban
  */
-public class DAOPersona {
-    
+public class DAOPersona implements IDAOPersona {
+
+    private Auditorio auditorio;
+
+    public DAOPersona(Auditorio auditorio) {
+        this.auditorio = auditorio;
+    }
+
+    public LSE<Persona> getPersonas() {
+        return auditorio.getPersonas();
+    }
+
+    @Override
+    public void agregarP(Persona persona) throws RuntimeException {
+        Persona aux = buscarP(persona.getCedula());
+        if (aux == null) {
+            auditorio.getPersonas().addFirst(persona);
+            Persistencia.getInstancia().escribirAuditorios();
+            return;
+        }
+        throw new RuntimeException("Ya se encuentra esa cedula");
+    }
+
+    @Override
+    public Persona buscarP(String cedula) {
+        for (int i = 0; i < auditorio.getPersonas().size(); i++) {
+            if (auditorio.getPersonas().get(i).getCedula().equals(cedula)) {
+                return auditorio.getPersonas().get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void editarP(Persona persona) throws RuntimeException {
+        Persona aux = buscarP(persona.getCedula());
+        if (aux != null) {
+            aux.setNombre(persona.getNombre());
+            Persistencia.getInstancia().escribirAuditorios();
+            return;
+        }
+        throw new RuntimeException("No se puede editar la persona");
+    }
+
+    @Override
+    public void eliminarP(String cedula) throws RuntimeException {
+        for (int i = 0; i < auditorio.getPersonas().size(); i++) {
+            if (auditorio.getPersonas().get(i).getCedula().equals(cedula)) {
+                auditorio.getPersonas().remove(i);
+                Persistencia.getInstancia().escribirAuditorios();
+                return;
+            }
+            throw new RuntimeException("No se puede eliminar la persona");
+        }
+    }
 }
